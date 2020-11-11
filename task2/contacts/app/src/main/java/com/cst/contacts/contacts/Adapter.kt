1676c99530894de8503cd0d392e44a1b6c44d05d
Seberyak <v_liparteliani@cu.edu.ge>
2020-11-12 03:05:52 +0400

    added design

--- task2/contacts/app/src/main/java/com/cst/contacts/contacts/Adapter.kt
+++ task2/contacts/app/src/main/java/com/cst/contacts/contacts/Adapter.kt
@@ -1,15 +1,18 @@
 package com.cst.contacts.contacts
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.cst.contacts.R
 import com.cst.contacts.details.ContactDetailedActivity
 import com.cst.contacts.donottouch.ContactInfo
-import com.cst.contacts.donottouch.Email
+import kotlin.random.Random
 
 
 class Adapter(private val contacts: List<ContactInfo>) :
@@ -18,13 +21,16 @@ class Adapter(private val contacts: List<ContactInfo>) :
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val vName: TextView =
             view.findViewById(R.id.contactNameId) as TextView
-        val vImage: TextView = view.findViewById(R.id.card_view_image) as TextView
-
+        val vImage: TextView = view.findViewById(R.id.mainFirstLetter) as TextView
+        val vShapeColor:ImageView = view.findViewById(R.id.mainShapeImage) as ImageView
+        var color:Int = 0
+//        val color: Int = vShapeColor.colorFilter.hashCode()
         init {
             itemView.setOnClickListener { v: View ->
                 val position: Int = adapterPosition
                 val intent: Intent = Intent(v.context, ContactDetailedActivity::class.java).apply {
                     putExtra("id", contacts[position].id)
+                    putExtra("shapeColor", color)
                 }
                 v.context.startActivity(intent)
             }
@@ -35,12 +41,18 @@ class Adapter(private val contacts: List<ContactInfo>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.vImage.text = contacts[position].name.substring(0, 1)
         holder.vName.text = contacts[position].name
+        holder.color = getRandomColor()
+        holder.vShapeColor.setColorFilter(holder.color)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.lsit_layout, parent, false)
         return MyViewHolder(itemView)
+    }
+    private fun getRandomColor(): Int {
+        return Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+
     }
 
     override fun getItemCount(): Int {
--- task2/contacts/app/src/main/java/com/cst/contacts/details/ContactDetailedActivity.kt
+++ task2/contacts/app/src/main/java/com/cst/contacts/details/ContactDetailedActivity.kt
@@ -1,58 +1,43 @@
 package com.cst.contacts.details
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.cst.contacts.R
 
 
 class ContactDetailedActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contac_details_layout)
 
         val id = intent.getLongExtra("id", 1)
+        val shapeColor = intent.getIntExtra("shapeColor", Color.BLUE)
         val result = ContactDetailedFragment().getContactById(id)
 
-        findViewById<TextView>(R.id.fistLetterImageId).apply {
-            text = result?.name?.substring(0, 1)
-        }
-        findViewById<TextView>(R.id.usernameId).apply {
-            text = result?.name
-        }
-        findViewById<TextView>(R.id.phoneNumberId).apply {
-            text = result?.phoneNumbers?.get(0)?.number
-        }
+        val shapeImage: ImageView = findViewById(R.id.shapeImageId)
+        shapeImage.setColorFilter(shapeColor)
 
-        findViewById<TextView>(R.id.emailId).apply {
 
-            if (result?.emails?.isNotEmpty()!!) {
-                text = result.emails[0].address
-            }
-        }
+        findViewById<TextView>(R.id.fistLetterImageId).text = result?.name?.substring(0, 1)
+
+
+        findViewById<TextView>(R.id.usernameId).text = result?.name
+
+        findViewById<TextView>(R.id.phoneNumberId).text = result?.phoneNumbers?.get(0)?.number
+
 
     }
 
-    fun back(view: View) {
-        onBackPressed()
-    }
-
-    fun call(view: View) {
-        Toast.makeText(this, "Calling", Toast.LENGTH_SHORT).show()
-    }
-
-    fun message(view: View) {
-        Toast.makeText(this, "Messaging", Toast.LENGTH_SHORT).show()
-    }
-
-    fun email(view: View) {
-        Toast.makeText(this, "Emailing", Toast.LENGTH_SHORT).show()
-    }
-
-    fun videoCall(view: View) {
-        Toast.makeText(this, "Video Calling", Toast.LENGTH_SHORT).show()
-    }
 }
